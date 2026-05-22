38db217a191c5ef43d27f1159a4a2856647c1d53
noelwilliams1 <noel.williams100@gmail.com>
2026-05-22 03:12:06 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="/aca.html"
+        title="ACA Script App"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none"
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
